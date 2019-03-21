c2c1e8bb471b7bc186a6128790598db5b534b56e
Andrea <andrea.impicciatore123@gmail.com>
2019-03-21 17:29:29 +0100

    Aggiunto Form

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from './../atom/Button';
-import Form from './../atom/Form;
+import Form from './../Componenti/Forms';
+import Checkbox from './../atom/Checkbox';
 
 export default class Login extends React.Component {
     render() {
