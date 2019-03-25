d0ed23e9fe6c8daecdc21b28a17d7951353ee8ca
byAmerigo <artbyAmerigo@gmail.com>
2019-03-25 13:00:05 +0100

    Foto profilo del messaggio

--- src/Componenti/SingleChat.js
+++ src/Componenti/SingleChat.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import './SingleChat.css'
+import ProfileImage from './../Atom/ProfileImage'
 export default class SingleChat extends React.Component {
 
     render() {
         return (
             <div className='singleChatDisplay'>
-                <div> Foto profilo </div>
-                <div> Stella dei preferiti </div>
-                <div> Nome Persona </div>
-                <div> Ultimo messaggio </div>
-                <div> Ora dell'ultimo Messaggio </div>
-                <div> Numero dei messaggi </div>
+                <ProfileImage preferito={true} />
+
             </div>
         )
     }
