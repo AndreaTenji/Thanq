034d8c7c515ed3a8081e07187e7d6ff0c7f79f74
byAmerigo <artbyAmerigo@gmail.com>
2019-03-25 11:32:55 +0100

    Aggiunto il welcome

---
+++ src/Atom/Quotes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Quotes.css'
+
+export default class Quotes extends React.Component {
+    render() {
+        return (
+            <div>
+                <p> Testo della quota</p>
+            </div>
+
+        )
+    }
+}
---
+++ src/Screen/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from './../Atom/Button';
+import Quotes from './../Atom/Quotes'
+
+import './Welcome.css'
+
+export default class Welcome extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2> Thanq</h2>
+
+                <Button titoloBottone='Welcome' />
+
+                <Quotes />
+            </div>
+
+        )
+    }
+}
