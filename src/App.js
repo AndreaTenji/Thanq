e5065f22935a3b1de2f6abdbced4a6bd8480cb46
Andrea <andrea.impicciatore123@gmail.com>
2019-03-25 16:10:08 +0100

    Header Componenti

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 import './App.css';
 //import Login from './Screen/Login.js';
 import SingleChat from './Componenti/SingleChat.js';
+import Header from './Componenti/Header.js';
 
 class App extends Component {
   render() {
     return (
+      <div>
+      <Header />
       <SingleChat />
+      </div>
     );
   }
 }
