94f8954c1ad7a22ccd4a1ac670d540ffcaa41f6a
Andrea <andrea.impicciatore123@gmail.com>
2019-03-25 17:49:54 +0100

    Bottone Cerca

--- src/Componenti/Header.js
+++ src/Componenti/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Header.css';
 export default class Header extends React.Component{
+    state = {
+        focus: false,
+    };
+    Cerca()
+    {
+        this.setState({
+            focus: true
+        })
+    }
     render(){
+        const CercaEl = this.state.focus ? 
+        <input type="text" className="ciao" onBlur={()=>this.setState({focus: false})} autoFocus/>
+            :
+            <img src={require('./../img/icon_search.svg')} onClick={()=>this.Cerca()} />
         return(
             <div className="background-one header">
                 <h2>ThanQ</h2>
-                <img src={require('./../img/icon_search.svg')} />
+                {CercaEl}
             </div>
         )
     }
