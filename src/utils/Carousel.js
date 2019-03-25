af3c1b16ebc11ec3e109e68f82041f09612a0f47
Flavia Capotombolo <flaviacapotombolo@gmail.com>
2019-03-25 11:33:42 +0100

    added

---
+++ src/utils/Carousel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Carousel from 'react-carousel';
+
+require('react-carousel/lib/carousel.css');
+
+render(
+    <Carousel viewportWidth="400px" cellPadding={5}>
+        <img src='' />
+        <img src='' />
+        <img src='' />
+    </Carousel>,
+    document.body
+);
