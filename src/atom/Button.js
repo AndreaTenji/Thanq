dc1f4a7c0a97070c970f8effcce6be6dc577e3f4
Flavia Capotombolo <flaviacapotombolo@gmail.com>
2019-03-25 10:52:55 +0100

    added props

--- src/atom/Button.js
+++ src/atom/Button.js
@@ -3,9 +3,10 @@ import './Button.css';
 
 export default class Button extends Component {
     render() {
+        const { titoloBottone } = this.props
         return (
             <div>
-                <button className="button background-two" type="submit" value="Props">PROPS</button>
+                <button className="button background-two" type="submit" value="Props">{titoloBottone}</button>
             </div>
         );
     }
--- src/screen/Login.js
+++ src/screen/Login.js
@@ -11,7 +11,7 @@ export default class Login extends React.Component {
             <div className='login-background'>
                 <Form />
                 <Checkbox />
-                <Button />
+                <Button titoloBottone='Login' />
             </div>
         )
     }
