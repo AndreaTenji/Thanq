cfd83fe9c8824915b2a46e032c85394fbcf10169
Flavia Capotombolo <flaviacapotombolo@gmail.com>
2019-03-25 17:39:04 +0100

    modify

--- src/Atom/Quotes.js
+++ src/Atom/Quotes.js
@@ -10,6 +10,8 @@ export default class Quotes extends React.Component {
                 <p> "Remember, teamwork begins by building trust. And the only way to do that is to overcome our need for invulnerability"
                     -Patrick Pancioni
                 </p>
+                <p>" Talent wins games, but teamwork and intelligence win championships" Micheal Jordan</p>
+                <p>" Coming together is a beginning. Keeping together is progress. Working together is success" Henry Ford</p>
             </div>
 
         )
