c1cc691348cf5ef1c3bf8d059c638031bc1fbf00
Flavia Capotombolo <flaviacapotombolo@gmail.com>
2019-03-25 12:18:54 +0100

    added image folder

--- src/Atom/Quotes.js
+++ src/Atom/Quotes.js
@@ -6,6 +6,7 @@ export default class Quotes extends React.Component {
     render() {
         return (
             <div>
+
                 <p> "Remember, teamwork begins by building trust. And the only way to do that is to overcome our need for invulnerability"
                     -Patrick Pancioni
                 </p>
