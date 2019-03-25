8a0a5574051ac153e8057de967c1ac7048a0f9d2
Flavia Capotombolo <flaviacapotombolo@gmail.com>
2019-03-25 15:09:58 +0100

    remove row 4 an 5 from welcome.js

--- src/Screen/Welcome.js
+++ src/Screen/Welcome.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from './../Atom/Button';
 import Quotes from './../Atom/Quotes';
-
 import './Welcome.css'
 
 export default class Welcome extends React.Component {
