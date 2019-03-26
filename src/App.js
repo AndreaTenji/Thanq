f5b20dc5255b4da9a5172433040629d29d27cea1
ketamik <stern@deathland.net>
2019-03-26 10:51:15 +0100

    inserito App.js su .gitignore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Login from './Screen/Login.js';
-import Welcome from './Screen/Welcome.js';
 //import Login from './Screen/Login.js';
-import SingleChat from './Componenti/SingleChat.js';
-import MenuTab from './Componenti/MenuTab';
-import Header from './Componenti/Header.js';
+import Welcome from './Screen/Welcome.js';
+import PasswordRecovery from './Screen/PasswordRecovery';
+//import Login from './Screen/Login.js';
+//import SingleChat from './Componenti/SingleChat.js';
+//import MenuTab from './Componenti/MenuTab';
+//import Header from './Componenti/Header.js';
 
 class App extends Component {
   render() {
@@ -14,9 +15,7 @@ class App extends Component {
 
       <div>
 
-        <Header />
-        <MenuTab />
-        <SingleChat />
+      <PasswordRecovery/>
 
       </div>
     );
