f311221627621e727b40e55d6403e505ab50c5ac
Flavia Capotombolo <flaviacapotombolo@gmail.com>
2019-03-26 09:51:44 +0100

    modify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Login from './Screen/Login.js';
+//import Login from './Screen/Login.js';
 import Welcome from './Screen/Welcome.js';
 //import Login from './Screen/Login.js';
-import SingleChat from './Componenti/SingleChat.js';
-import MenuTab from './Componenti/MenuTab';
-import Header from './Componenti/Header.js';
+//mport SingleChat from './Componenti/SingleChat.js';
+//import MenuTab from './Componenti/MenuTab';
+//import Header from './Componenti/Header.js';
 
 class App extends Component {
   render() {
@@ -14,9 +14,8 @@ class App extends Component {
 
       <div>
 
-        <Header />
-        <MenuTab />
-        <SingleChat />
+        <Welcome />
+
 
       </div>
     );
