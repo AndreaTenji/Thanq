98fd264e520ce97fa57a8652063d6f4047b8d329
ketamik <stern@deathland.net>
2019-03-28 10:42:09 +0100

    Name changed on the Profile state

--- src/Screen/Profile.js
+++ src/Screen/Profile.js
@@ -11,7 +11,7 @@ export default class Profile extends React.Component {
         super(props)
         this.state = {
             Name: 'Roberto',
-            Surname: 'Manchi',
+            Surname: 'Greco',
             Job: 'Social Media',
             ImageProfileURL: 'Roberto',
         }
