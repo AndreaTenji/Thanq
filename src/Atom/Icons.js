a7a3e8a38df096f63dd529c41ec4a2bf8c107bc6
byAmerigo <artbyAmerigo@gmail.com>
2019-03-28 11:39:04 +0100

    Icons Component

--- src/Atom/Icons.js
+++ src/Atom/Icons.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import Back from './../Assets/icons/icon_next.svg'
 import Job from './../Assets/icons/icon_job.svg'
+import Attachment from './../Assets/icons/icon_attachment.svg'
+import Cancel from './../Assets/icons/icon_cancel.svg'
+
+
+
+export class IconAttachment extends React.Component {
+    render() {
+        const { size } = this.props
+        return (
+            <img
+                src={Attachment}
+                alt="Attachment"
+                width={size}
+                height={size}
+
+            />
+        )
+    }
+}
 
 export class IconBack extends React.Component {
     render() {
@@ -14,6 +33,22 @@ export class IconBack extends React.Component {
         )
     }
 }
+
+export class IconCancel extends React.Component {
+    render() {
+        const { size } = this.props
+        return (
+            <img
+                src={Cancel}
+                alt="Cancel"
+                width={size}
+                height={size}
+
+            />
+        )
+    }
+}
+
 
 export class IconJob extends React.Component {
     render() {
@@ -29,5 +64,4 @@ export class IconJob extends React.Component {
         )
     }
 }
-
 
