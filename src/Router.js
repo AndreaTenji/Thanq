a8ab944244a3cbfc88894eb63fc31305a4d8289d
byAmerigo <artbyamerigo@gmail.com>
2019-03-31 01:29:01 +0100

    Aggiunta la chat che ora si basa al database falso

--- src/Router.js
+++ src/Router.js
@@ -2,20 +2,24 @@ import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import LandingPage from "./Screen/LandingPage";
 import Chat from "./Screen/Chat";
+import Prova from "./Prova";
+import ErrorScreen from "./Screen/Error";
 export default class Routes extends React.Component {
 
     render() {
 
         return (
             <div>
+                <Route exact path="/" render={() => <LandingPage screen={'Messages'} messages={this.props.messages} />} />
                 <Route exact path="/chat/" render={() => <Redirect to='/messages/' />} />
-                <Route exact path="/settings/" render={() => <Redirect to='/profile/' />} />
-                <Route path="/chat/:id" component={Chat} />
+                <Route path="/settings/" render={() => <Redirect to='/profile/' />} />
+                <Route path="/prova/:vediamo" render={(props) => <Prova {...props} testo={'ahahah'} />} />
+                <Route path="/chat/:id" render={(props) => <Chat {...props} data={this.props.data} />} />
                 <Route path="/messages/" render={() => <LandingPage screen={'Messages'} messages={this.props.messages} />} />
                 <Route path="/favorite/" render={() => <LandingPage screen={'Favorite'} messages={this.props.messages} />} />
                 <Route path="/profile/" component={() => <LandingPage screen={'Profile'} messages={this.props.messages} />} />
-                <Route exact path="/" render={() => <LandingPage screen={'Messages'} messages={this.props.messages} />} />
-                <Route render={() => <div> 404 </div>} />
+                <Route exact path="/error/" component={ErrorScreen} />
+                {/* <Route render={() => <div> 404 </div>} /> */}
             </div>
         )
     }
--- src/Screen/Chat.js
+++ src/Screen/Chat.js
@@ -2,50 +2,81 @@ import React from 'react';
 import ChatHeader from './../Componenti/ChatHeader'
 import SingleMessage from './../Atom/SingleMessage'
 import ChatInput from './../Componenti/ChatInput'
+import { Redirect } from 'react-router-dom'
+
+
 
 export default class Chat extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            id: 123,
-            name: 'Loredana',
-            surname: 'Bee',
-            jobRole: 'Media Social',
-            imageProfile: 'image-katia123456',
-            favorite: false,
-            messages: { orario: '12:02', messaggio: 'Proviamo', type: true },
+            favorite: null,
         }
     }
 
+    // componentDidMount() {
+    //     let numId = null
+    //     const { data } = this.props
+
+    //     for (let i = 0; i < data.contactsChat.length; i++) {
+    //         if (data.contactsChat[i].id == this.props.match.params.id) {
+    //             numId = i
+    //             break
+    //         }
+    //     }
+
+    //     this.setState({
+    //         favorite: this.props.data.contactsChat[numId].favourite
+    //     })
+    // }
+
     render() {
+        let numId = null
+        const { data } = this.props
+
+        for (let i = 0; i < data.contactsChat.length; i++) {
+            if (data.contactsChat[i].id == this.props.match.params.id) {
+                numId = i
+                break
+            }
+        }
+
+        const id = data.contactsChat[numId]
+
+        if (numId == null) {
+            return <Redirect to='/error/' />
+        }
+
         return (
             <div>
                 <div style={{ height: ' 100vh', backgroundColor: 'white' }}>
                     <div style={{ position: 'fixed', width: '100vw' }}>
                         <ChatHeader
-                            name={this.props.match.params.id}
-                            surname={this.state.surname}
-                            jobRole={this.state.jobRole}
+                            // name={prova}
+                            name={id.name}
+                            surname={id.surname}
+                            jobRole={id.job}
                             favorite={this.state.favorite}
-                            actionFavorite={() => alert('prova')}
-                            imageProfileURL={this.state.imageProfile}
+                            actionFavorite={() => {
+                                id.favorite = !id.favorite;
+                                this.setState({ favorite: id.favorite })
+                            }}
+                            imageProfileURL={id.profileImageURL}
                         />
                     </div>
-                    <div style={{ paddingLeft: '10px', paddingRight: '10px', marginBottom: '80px' }}>
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo'} type={true} />
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo'} type={true} />
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo, fnoasfnsfafs fsfasfsaffsafaso'} type={false} />
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo'} type={true} />
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo'} type={true} />
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo'} type={true} />
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo'} type={true} />
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo'} type={true} />
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo'} type={true} />
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo'} type={true} />
-                        <SingleMessage orario='12:02' messaggio={'Proviamo di nuovo'} type={true} />
+                    <div style={{ paddingLeft: '10px', paddingRight: '10px', paddingTop: '80px', paddingBottom: '100px' }}>
+
+                        {id.messages.map((el, i) =>
+                            <SingleMessage
+                                key={i}
+                                orario={el.time}
+                                messaggio={el.text}
+                                type={el.sender == id.id} />
+                        )
+                        }
                     </div>
                 </div>
-                <div style={{ position: 'fixed', top: '85vh' }}>
+                <div style={{ position: 'fixed', top: '90vh' }}>
                     <ChatInput />
                 </div>
             </div >
