d3a1d8584ad3b403b869ca6ae1406009ecad1b4f
Flavia Capotombolo <flaviacapotombolo@gmail.com>
2019-04-01 10:08:03 +0200

    Added comment for date

--- src/Screen/Chat.js
+++ src/Screen/Chat.js
@@ -64,3 +64,123 @@ export default class Chat extends React.Component {
         )
     }
 }
+
+
+//funzione per la data e orario modo 1
+/* const padStart = _.padStart;
+
+class SidebarContent extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  getHours() {
+    return padStart(this.state.date.getHours(), 2, '0');
+  }
+  getMinutes() {
+    return padStart(this.state.date.getMinutes(), 2, '0');
+  }
+
+  getSeconds() {
+    return padStart(this.state.date.getSeconds(), 2, '0');
+  }
+
+
+  componentDidMount() {
+
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  render() {
+
+    return ( <
+      ul className = "nav"
+      ref = {
+        (c) => {
+          this.nav = c;
+        }
+      } >
+      <
+      li className = "today" >
+      <
+      div className = "clock"
+      center = "xs" >
+      <
+      div center = "xs" >
+      <
+      span className = "hours" > {
+        this.getHours()
+      } < /span> <
+      span >: < /span> <
+      span className = "min" > {
+        this.getMinutes()
+      } < /span> <
+      span >: < /span> <
+      span className = "sec" > {
+        this.getSeconds()
+      } < /span> <
+      /div> <
+      /div> <
+      div className = "date"
+      center = "xs" >
+      <
+      p className = "today-is" > {
+        this.state.date.toDateString()
+      } < /p> <
+      /div> <
+      /li> <
+      /ul>
+    );
+  }
+}
+
+
+ReactDOM.render( < SidebarContent / > , document.getElementById("app"));
+}
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.min.js"></script>
+<script src="https://cdn.jsdelivr.net/lodash/4.17.4/lodash.min.js"></script>
+
+<div id="app" />
+
+
+*/
+
+
+
+//funzione per la data e orario modo 2
+/* export default class Chat extends React.Component {
+
+    state = {
+        date=""
+    };
+
+    componentDidMount(){
+        this.getDate();
+    }
+
+    getDate = () => {
+        var date = new Date().toDateString();
+        this.setState({ date });
+    };
+
+    render () {
+        const{date} = this.state;
+        return<div>{date}</div>;
+    }
+
+}*/
