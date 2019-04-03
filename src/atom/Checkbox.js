1512bcb023795b62a5fe08975cbca30c2577af92
byAmerigo <artbyAmerigo@gmail.com>
2019-04-03 16:42:30 +0200

    Aggiustato checkbox

--- src/atom/Checkbox.js
+++ src/atom/Checkbox.js
@@ -6,7 +6,9 @@ export default class Login extends React.Component {
         return (
             <div className='display'>
                 <input type="checkbox" name="checkboxG1" id="checkboxG1" className="css-checkbox" />
-                <label for="checkboxG1" className="css-label"><h3>Remember Me </h3></label>
+                <label for="checkboxG1" className="css-label">
+                    <h3>Remember Me </h3>
+                </label>
             </div>
         )
     }
