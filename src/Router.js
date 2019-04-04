72183e3fd59b297fa5a82ce8aa3056a7602b7518
byAmerigo <artbyAmerigo@gmail.com>
2019-04-04 12:27:21 +0200

    Responsive, non finito

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,16 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import PropsTypes from 'prop-types'
+import MediaQuery from 'react-responsive';
+
 import LandingPage from "./Screen/LandingPage";
 import Chat from "./Screen/Chat";
 import Prova from "./Prova";
 import ErrorScreen from "./Screen/Error";
 import Login from './Screen/Login'
+import MenuTab from "./Componenti/MenuTab";
+import Header from "./Componenti/Header";
+import Messages from "./Screen/Messages";
 export default class Routes extends React.Component {
 
     render() {
@@ -14,24 +19,35 @@ export default class Routes extends React.Component {
         if (login) {
             return (
                 <div>
-                    <Route exact path="/" render={() => <LandingPage screen={'Messages'} messages={messages} />} />
-                    <Route exact path="/chat/" render={() => <Redirect to='/messages/' />} />
-                    <Route path="/settings/" render={() => <Redirect to='/profile/' />} />
-                    <Route path="/prova/:vediamo" render={(props) => <Prova {...props} testo={'ahahah'} />} />
-                    <Route path="/chat/:id" render={(props) => <Chat {...props} data={data} />} />
-                    <Route path="/messages/" render={() => <LandingPage screen={'Messages'} messages={messages} />} />
-                    <Route path="/favorite/" render={() => <LandingPage screen={'Favorite'} messages={messages} />} />
-                    <Route path="/profile/" render={() => <LandingPage screen={'Profile'} profile={profile} logout={logout} />} />
-                    <Route exact path="/error/" component={ErrorScreen} />
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <Route render={() => <Login authLogin={authLogin} />} />
+                    <MediaQuery query="(max-width: 900px)">
+                        <Route exact path="/" render={() => <LandingPage screen={'Messages'} messages={messages} />} />
+                        <Route exact path="/chat/" render={() => <Redirect to='/messages/' />} />
+                        <Route path="/settings/" render={() => <Redirect to='/profile/' />} />
+                        <Route path="/prova/:vediamo" render={(props) => <Prova {...props} testo={'ahahah'} />} />
+                        <Route path="/chat/:id" render={(props) => <Chat {...props} data={data} />} />
+                        <Route path="/messages/" render={() => <LandingPage screen={'Messages'} messages={messages} />} />
+                        <Route path="/favorite/" render={() => <LandingPage screen={'Favorite'} messages={messages} />} />
+                        <Route path="/profile/" render={() => <LandingPage screen={'Profile'} profile={profile} logout={logout} />} />
+                        <Route exact path="/error/" component={ErrorScreen} />
+
+                    </MediaQuery>
+
+                    <MediaQuery query="(min-width: 900px)">
+                        <Route exact path="/" render={() => <LandingPage screen={'Favorite'} messages={messages} />} />
+                        <Route path="/messages/" render={() => <LandingPage screen={'Messages'} messages={messages} />} />
+                        <Route path="/favorite/" render={() => <LandingPage screen={'Favorite'} messages={messages} />} />
+                        <Route path="/profile/" render={() => <LandingPage screen={'Profile'} profile={profile} logout={logout} />} />
+                    </MediaQuery>
+
                 </div>
             )
         }
+
+        return (
+            <div>
+                <Route render={() => <Login authLogin={authLogin} />} />
+            </div>
+        )
     }
 }
 
--- src/Screen/LandingPage.js
+++ src/Screen/LandingPage.js
@@ -4,6 +4,8 @@ import MenuTab from './../Componenti/MenuTab'
 import Messages from './../Screen/Messages'
 import Favorite from './../Screen/Favorite'
 import Profile from './../Screen/Profile'
+import MediaQuery from 'react-responsive';
+
 
 export default class LandingPage extends React.Component {
 
@@ -17,19 +19,54 @@ export default class LandingPage extends React.Component {
 
         return (
             < div >
-                <div style={{ position: "fixed", zIndex: 3 }}>
-                    <Header />
-                    <MenuTab
-                        currentView={this.props.screen}
-                        actionTab={(x) => this.setState({ currentScreen: x })}
-                    />
-                </div>
+                <MediaQuery query="(max-width: 900px)">
+                    <div style={{ position: "fixed", zIndex: 3 }}>
+                        <Header />
+                        <MenuTab
+                            currentView={this.props.screen}
+                            actionTab={(x) => this.setState({ currentScreen: x })}
+                        />
+                    </div>
 
-                <div style={{ backgroundColor: '#C39BE1', paddingTop: '120px' }}>
-                    {this.props.screen === 'Messages' && <Messages messages={this.props.messages} actionSingleChat={(x) => this.props.viewScreen(x)} />}
-                    {this.props.screen === 'Favorite' && <Favorite messages={this.props.messages} actionSingleChat={(x) => this.props.viewScreen(x)} />}
-                    {this.props.screen === 'Profile' && <Profile profile={this.props.profile} logout={this.props.logout} actionSetting={(x) => this.props.viewScreen(x)} />}
-                </div>
+                    <div style={{ backgroundColor: '#C39BE1', paddingTop: '120px' }}>
+                        {this.props.screen === 'Messages' && <Messages messages={this.props.messages} actionSingleChat={(x) => this.props.viewScreen(x)} />}
+                        {this.props.screen === 'Favorite' && <Favorite messages={this.props.messages} actionSingleChat={(x) => this.props.viewScreen(x)} />}
+                        {this.props.screen === 'Profile' && <Profile profile={this.props.profile} logout={this.props.logout} actionSetting={(x) => this.props.viewScreen(x)} />}
+                    </div>
+                </MediaQuery>
+
+                <MediaQuery query="(min-width: 900px)">
+                    <div style={{ display: 'flex' }}>
+
+                        <MenuTab
+                            currentView={this.props.screen}
+                            actionTab={(x) => this.setState({ currentScreen: x })}
+                        />
+
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+
+                            <div style={{ position: "fixed", zIndex: 3 }}>
+                                <Header />
+                            </div>
+                            <div style={{ backgroundColor: '#C39BE1', paddingTop: '70px' }}>
+                                {this.props.screen === 'Messages' && <Messages messages={this.props.messages} actionSingleChat={(x) => this.props.viewScreen(x)} />}
+                                {this.props.screen === 'Favorite' && <Favorite messages={this.props.messages} actionSingleChat={(x) => this.props.viewScreen(x)} />}
+                                {this.props.screen === 'Profile' && <Profile profile={this.props.profile} logout={this.props.logout} actionSetting={(x) => this.props.viewScreen(x)} />}
+                            </div>
+
+                        </div>
+                        <div style={{ flex: 1, backgroundColor: 'red' }}>
+                            Prova
+                            </div>
+                    </div>
+
+
+
+
+                </MediaQuery>
+
+
+
             </div >
 
         )
