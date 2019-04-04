e61d7b00670eb214923291c7ed30f24fa88caecc
Andrea <andrea.impicciatore123@gmail.com>
2019-04-04 15:27:30 +0200

    Db inserito e presso id di chi entra

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,11 +1,12 @@
-import { initializeApp } from 'firebase'
+import Firebase from 'firebase'
 // Initialize Firebase
-const config = {
+Firebase.initializeApp({
     apiKey: "AIzaSyBDT1WCYrEu0BmEa1ZXZ4fVtNXhrbq3ahk",
     authDomain: "thanq-c41af.firebaseapp.com",
     databaseURL: "https://thanq-c41af.firebaseio.com",
     projectId: "thanq-c41af",
     storageBucket: "thanq-c41af.appspot.com",
     messagingSenderId: "297767299087"
-};
-export const Firebase = initializeApp(config);
+});
+export const firebase = Firebase;
+export var db = Firebase.firestore()
