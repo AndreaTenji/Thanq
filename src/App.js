e3d7af3308ee5965c65b57b36636600724b5e70b
byAmerigo <artbyAmerigo@gmail.com>
2019-04-08 10:59:42 +0200

    Fix login, c'è da finire firebase

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
 
   constructor(props) {
     super(props)
-
+    this.loginEmail = this.loginEmail.bind(this)
     this.state = {
       id: null,
       login: false,
@@ -20,18 +20,19 @@ export default class App extends Component {
       profile: null,
     }
   }
-  // componentDidMount() {
 
-  //   if (this.state.login) {
-  //     getProfile(this.state.id)
-  //       .then(profile => this.setState({ profile: profile }))
-  //   }
 
-  // }
-
-  login(email, password) {
+  loginEmail(email, password) {
     Firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => this.setState({ id: user.user.uid }))
+      .then(user => {
+        this.setState({ id: user.user.uid, login: true, });
+        console.log('UID = ' + this.state.id);
+        getProfile(this.state.id)
+          .then(profile => {
+            this.setState({ profile: profile });
+            console.log('PROFILO = ' + this.state.profile)
+          })
+      })
   }
 
   render() {
@@ -40,7 +41,7 @@ export default class App extends Component {
         <Router >
           <Route
             logout={() => this.setState({ login: false, })}
-            authLogin={this.login}
+            authLogin={this.loginEmail}
             login={this.state.login}
             profile={this.state.data}
             data={this.state.data}
